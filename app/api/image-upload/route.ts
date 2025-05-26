4e327b0737b03e64b96a864df0b9c0a07cdf5846
itssoumodip <soumodipddas2005@gmail.com>
2025-05-27 00:57:10 +0530

    add cloudinary integration for image upload and optimization

---
+++ app/api/image-upload/route.ts
@@ -0,0 +1,64 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { NextRequest, NextResponse } from 'next/server';
+import { auth } from '@clerk/nextjs/server';
+
+// Configuration
+cloudinary.config({
+    cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+interface cloudinaryUploadResult {
+    public_id: string;
+    [key: string]: any;
+}
+
+export async function POST (request: NextRequest) {
+    const {userId} = auth();
+    if (!userId) {
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
+    try {
+        const formData = await request.formData();
+        const file = formData.get('file') as File | null;
+        if (!file) {
+            return NextResponse.json({ error: 'File not found' }, { status: 400 });
+        }
+
+    } catch (error) {
+        
+    }
+}
+    // Upload an image
+    const uploadResult = await cloudinary.uploader
+        .upload(
+            'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+            public_id: 'shoes',
+        }
+        )
+        .catch((error) => {
+            console.log(error);
+        });
+
+    console.log(uploadResult);
+
+    // Optimize delivery by resizing and applying auto-format and auto-quality
+    const optimizeUrl = cloudinary.url('shoes', {
+        fetch_format: 'auto',
+        quality: 'auto'
+    });
+
+    console.log(optimizeUrl);
+
+    // Transform the image: auto-crop to square aspect_ratio
+    const autoCropUrl = cloudinary.url('shoes', {
+        crop: 'auto',
+        gravity: 'auto',
+        width: 500,
+        height: 500,
+    });
+
+    console.log(autoCropUrl);
+})();
